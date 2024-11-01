f3a35766e2db74a15be2c4bda238268c970e33af
DEKEL USACH <48386383@ort>
2024-11-01 15:45:41 -0300

    Finished RestauranteElegido.cshtml

--- Models/Comida.cs
+++ Models/Comida.cs
@@ -6,5 +6,7 @@ public int IdCategoriaComida{get; set;}
 public string Nombre {get; set;}
 public string Descripcion {get; set;}
 
+public string Imagen {get; set;}
+public int Precio {get; set;}
 
 }
