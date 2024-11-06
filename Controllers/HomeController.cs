85718cd4a62e4a168eee037be58b049636803d02
DEKEL USACH <48386383@ort>
2024-11-06 09:32:08 -0300

    Added carrito

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,16 +15,17 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-            ViewBag.Restaurantes = BD.ObtenerRestaurantes();
+        ViewBag.Restaurantes = BD.ObtenerRestaurantes();
         return View();
     }
 
- 
+
     public IActionResult Mapa()
     {
         return View();
     }
-    public IActionResult Perfil(){
+    public IActionResult Perfil()
+    {
         ViewBag.usuario = BD.ObtenerInfoUsuario(1);
         return View();
     }
@@ -45,11 +46,37 @@ public class HomeController : Controller
         ViewBag.Comida = BD.ObtenerComidasDeRestauranteElegido(IdRestaurante);
         return View("RestauranteElegido");
     }
-   
+    [HttpPost]
+    public IActionResult Comprar(Comida comidaElegida, int IdRestaurante)
+    {
+        ViewBag.Restaurante = BD.ObtenerRestaurantesElegido(IdRestaurante);
+        ViewBag.Comida = BD.ObtenerComidasDeRestauranteElegido(IdRestaurante);
+        Comida.carrito.Add(comidaElegida);
+        return View("RestauranteElegido");
+    }
 
-public IActionResult Nosotros(){
-    return View();
-}
+    public IActionResult Carrito()
+    {
+        ViewBag.ListaComida = Comida.carrito;
+        return View();
+    }
+    [HttpPost]
+    public IActionResult EliminarCarrito(int IdComida)
+    {
+        var itemToRemove = Comida.carrito.FirstOrDefault(c => c.IdComida == IdComida);
+
+        if (itemToRemove != null)
+        {
+            Comida.carrito.Remove(itemToRemove);
+        }
+
+        return View("Carrito");
+    }
+
+    public IActionResult Nosotros()
+    {
+        return View();
+    }
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
