fc2c9f1491006c3936d07faed7eaa9fc2a848320
DEKEL USACH <48386383@ort>
2024-11-13 12:05:47 -0300

    Fixed index cards

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,12 +2,18 @@
     var errorMsg = document.querySelector('.error-msg');
     
     if (errorMsg) {
+        // Primero ocultamos el mensaje de error (opcional)
+        errorMsg.style.transform = 'scale(0)';
+        errorMsg.style.opacity = 0;
+
+        // Animación de expansión
         anime({
-            targets: '.error-msg',            
-            width: '100%', 
-            easing: 'easeInOutQuad',
-            direction: 'alternate'
-        
+            targets: '.error-msg',
+            scale: [0, 1],    // Expande desde el centro
+            opacity: [0, 1],  // También aumentamos la opacidad
+            easing: 'easeOutQuad',
+            duration: 10   // Duración de la animación
+                // Retardo opcional
         });
     }
 });
