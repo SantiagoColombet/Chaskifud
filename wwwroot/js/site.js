391a06fa96e80751392646493af5fff4f3456fd2
DEKEL USACH <48386383@ort>
2024-12-05 13:33:25 -0300

    index

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -146,3 +146,29 @@ user.addEventListener("click", () =>{
 // }
 
 
+
+
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 0,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 0,
+        },
+    },
+});
